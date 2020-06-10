4ee24081b091e6c80d5bdbdaff197b1673c51739
Antonio Vazquez Brust <bitsandbricks@users.noreply.github.com>
2020-06-10 13:51:49 -0300

    Update theme_fbb.R

--- R/theme_fbb.R
+++ R/theme_fbb.R
@@ -45,3 +45,18 @@ theme_fbb <- function(base_size = 14,
             complete = TRUE
         )
 }
+
+
+theme_fbb_map <- function(base_size = 14,
+                      base_family = "CircularStd",
+                      base_line_size = base_size / 170,
+                      base_rect_size = base_size / 170) {
+  theme_fbb(base_size, base_family, base_line_size, base_rect_size) %+replace% 
+        theme(axis.line = element_blank(), axis.text = element_blank(), 
+            axis.ticks = element_blank(), axis.title = element_blank(), 
+            panel.background = element_blank(), panel.border = element_blank(), 
+            panel.grid = element_blank(), panel.spacing = unit(0, 
+                "lines"), plot.background = element_blank(), 
+            legend.justification = c(0, 0), legend.position = c(0, 
+                0))
+  }
