4b98f1264c6d84331c3ee15449307bb9ff5c19d5
havb <avazquez@gmail.com>
2021-06-11 20:29:13 -0300

    ajuste a theme_fbb()

--- R/theme_fbb.R
+++ R/theme_fbb.R
@@ -36,11 +36,11 @@ theme_fbb <- function(base_size = 14,
                 size = rel(0.5)),
             panel.grid.major = element_line(
                 rgb(105, 105, 105, maxColorValue = 255),
-                linetype = "dotted"),
-            panel.grid.minor = element_line(
-                rgb(105, 105, 105, maxColorValue = 255),
                 linetype = "dotted",
                 size = rel(4)),
+            panel.grid.minor = element_line(
+                rgb(105, 105, 105, maxColorValue = 255),
+                linetype = "dotted"),
 
             complete = TRUE
         )
@@ -52,12 +52,12 @@ theme_fbb_map <- function(base_size = 14,
                       base_family = "CircularStd",
                       base_line_size = base_size / 170,
                       base_rect_size = base_size / 170) {
-  theme_fbb(base_size, base_family, base_line_size, base_rect_size) %+replace% 
-        theme(axis.line = element_blank(), axis.text = element_blank(), 
-            axis.ticks = element_blank(), axis.title = element_blank(), 
-            panel.background = element_blank(), panel.border = element_blank(), 
-            panel.grid = element_blank(), panel.spacing = unit(0, 
-                "lines"), plot.background = element_blank(), 
-            legend.justification = c(0, 0), legend.position = c(0, 
+  theme_fbb(base_size, base_family, base_line_size, base_rect_size) %+replace%
+        theme(axis.line = element_blank(), axis.text = element_blank(),
+            axis.ticks = element_blank(), axis.title = element_blank(),
+            panel.background = element_blank(), panel.border = element_blank(),
+            panel.grid = element_blank(), panel.spacing = unit(0,
+                "lines"), plot.background = element_blank(),
+            legend.justification = c(0, 0), legend.position = c(0,
                 0))
   }
